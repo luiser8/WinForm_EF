b98d71a1581e2048bbebf4bd44b8917e774dce7c
Luis E. Rondon <rondonle@avior.com.ve>
2018-07-20 15:58:34 -0400

    20-07-2018 15:58pm

---
+++ Forms/Productos.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Facturacion
+{
+    partial class Factura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ProductoId = new System.Windows.Forms.NumericUpDown();
+            this.ProductoNom = new System.Windows.Forms.TextBox();
+            this.ProductoDesc = new System.Windows.Forms.TextBox();
+            this.ProductoPrecio = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ProductoEstado = new System.Windows.Forms.CheckBox();
+            this.ProductoStock = new System.Windows.Forms.NumericUpDown();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnBuscarPorNombre = new System.Windows.Forms.Button();
+            this.btnBuscarFiltro = new System.Windows.Forms.Button();
+            this.dgvRecords = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductoId)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductoStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Productos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Producto ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Descripcion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Precio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Stock";
+            // 
+            // ProductoId
+            // 
+            this.ProductoId.Location = new System.Drawing.Point(83, 47);
+            this.ProductoId.Name = "ProductoId";
+            this.ProductoId.Size = new System.Drawing.Size(120, 20);
+            this.ProductoId.TabIndex = 0;
+            // 
+            // ProductoNom
+            // 
+            this.ProductoNom.Location = new System.Drawing.Point(83, 75);
+            this.ProductoNom.Name = "ProductoNom";
+            this.ProductoNom.Size = new System.Drawing.Size(120, 20);
+            this.ProductoNom.TabIndex = 1;
+            // 
+            // ProductoDesc
+            // 
+            this.ProductoDesc.Location = new System.Drawing.Point(83, 106);
+            this.ProductoDesc.Name = "ProductoDesc";
+            this.ProductoDesc.Size = new System.Drawing.Size(120, 20);
+            this.ProductoDesc.TabIndex = 2;
+            // 
+            // ProductoPrecio
+            // 
+            this.ProductoPrecio.Location = new System.Drawing.Point(83, 132);
+            this.ProductoPrecio.Name = "ProductoPrecio";
+            this.ProductoPrecio.Size = new System.Drawing.Size(120, 20);
+            this.ProductoPrecio.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Estado";
+            // 
+            // ProductoEstado
+            // 
+            this.ProductoEstado.AutoSize = true;
+            this.ProductoEstado.Location = new System.Drawing.Point(83, 190);
+            this.ProductoEstado.Name = "ProductoEstado";
+            this.ProductoEstado.Size = new System.Drawing.Size(56, 17);
+            this.ProductoEstado.TabIndex = 5;
+            this.ProductoEstado.Text = "Activo";
+            this.ProductoEstado.UseVisualStyleBackColor = true;
+            this.ProductoEstado.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // ProductoStock
+            // 
+            this.ProductoStock.Location = new System.Drawing.Point(83, 164);
+            this.ProductoStock.Name = "ProductoStock";
+            this.ProductoStock.Size = new System.Drawing.Size(120, 20);
+            this.ProductoStock.TabIndex = 4;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(16, 230);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(106, 230);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(196, 230);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 8;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(288, 230);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 9;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(382, 230);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnBuscarPorNombre
+            // 
+            this.btnBuscarPorNombre.Location = new System.Drawing.Point(16, 271);
+            this.btnBuscarPorNombre.Name = "btnBuscarPorNombre";
+            this.btnBuscarPorNombre.Size = new System.Drawing.Size(199, 23);
+            this.btnBuscarPorNombre.TabIndex = 11;
+            this.btnBuscarPorNombre.Text = "Buscar por nombre";
+            this.btnBuscarPorNombre.UseVisualStyleBackColor = true;
+            this.btnBuscarPorNombre.Click += new System.EventHandler(this.btnBuscarPorNombre_Click);
+            // 
+            // btnBuscarFiltro
+            // 
+            this.btnBuscarFiltro.Location = new System.Drawing.Point(244, 271);
+            this.btnBuscarFiltro.Name = "btnBuscarFiltro";
+            this.btnBuscarFiltro.Size = new System.Drawing.Size(213, 23);
+            this.btnBuscarFiltro.TabIndex = 12;
+            this.btnBuscarFiltro.Text = "Filtar nombre";
+            this.btnBuscarFiltro.UseVisualStyleBackColor = true;
+            this.btnBuscarFiltro.Click += new System.EventHandler(this.btnBuscarFiltro_Click);
+            // 
+            // dgvRecords
+            // 
+            this.dgvRecords.AllowUserToAddRows = false;
+            this.dgvRecords.AllowUserToDeleteRows = false;
+            this.dgvRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecords.Location = new System.Drawing.Point(232, 47);
+            this.dgvRecords.Name = "dgvRecords";
+            this.dgvRecords.ReadOnly = true;
+            this.dgvRecords.Size = new System.Drawing.Size(382, 160);
+            this.dgvRecords.TabIndex = 13;
+            this.dgvRecords.DoubleClick += new System.EventHandler(this.dgvRecords_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Clientes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Factura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 317);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvRecords);
+            this.Controls.Add(this.btnBuscarFiltro);
+            this.Controls.Add(this.btnBuscarPorNombre);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.ProductoStock);
+            this.Controls.Add(this.ProductoEstado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ProductoPrecio);
+            this.Controls.Add(this.ProductoDesc);
+            this.Controls.Add(this.ProductoNom);
+            this.Controls.Add(this.ProductoId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Factura";
+            this.Text = ".:.Facturacion.:.";
+            this.Load += new System.EventHandler(this.Factura_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductoId)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductoStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecords)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown ProductoId;
+        private System.Windows.Forms.TextBox ProductoNom;
+        private System.Windows.Forms.TextBox ProductoDesc;
+        private System.Windows.Forms.TextBox ProductoPrecio;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox ProductoEstado;
+        private System.Windows.Forms.NumericUpDown ProductoStock;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnBuscarPorNombre;
+        private System.Windows.Forms.Button btnBuscarFiltro;
+        private System.Windows.Forms.DataGridView dgvRecords;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Forms/Productos.cs
@@ -0,0 +1,298 @@
+﻿using Facturacion.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Facturacion
+{
+    public partial class Factura : Form
+    {
+        //Instancia context ef
+        Models.FacturacionEntities db = new Models.FacturacionEntities();
+
+        public Factura()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Cuando carga el form
+        private void Factura_Load(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        //Boton guardar
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Models.Producto producto = new Models.Producto();
+                producto.Nombre = ProductoNom.Text;
+                producto.Descripcion = ProductoDesc.Text;
+                producto.Precio = ProductoPrecio.Text;
+                producto.Stock = (int)ProductoStock.Value;
+                producto.Estado = ProductoEstado.Checked;
+                producto.Fecha = DateTime.Now;
+
+                db.Productos.Add(producto);
+                db.SaveChanges();
+
+                MessageBox.Show("Producto guardado!");
+                ResetForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Limpiar cajas de texto
+        private void ResetForm()
+        {
+            ProductoNom.Text = string.Empty;
+            ProductoDesc.Text = string.Empty;
+            ProductoPrecio.Text = string.Empty;
+            ProductoStock.Value = 0.0M;
+            ProductoEstado.Checked = true;
+
+            LoadAllProductos();
+        }
+
+        //Llena el datagrid
+        private void LoadAllProductos()
+        {
+            try
+            {
+                Models.Producto producto = new Models.Producto();
+                var query = from prod in db.Productos
+                            orderby prod.ProductoId ascending
+                            select prod;
+                //dgvRecords.DataSource = db.Productos.ToList();
+                dgvRecords.DataSource = query.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Llama ventana clientes
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clientes clientes = new Clientes();
+            clientes.ShowDialog();
+        }
+
+        //Limpiar
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        //Buscar por id
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Models.Producto producto = new Models.Producto();
+                var query = from prod in db.Productos
+                            where prod.ProductoId == (int)ProductoId.Value
+                            select prod;
+
+                Models.Producto productoId = query.SingleOrDefault();
+
+                if (productoId != null)
+                {
+                    ProductoNom.Text = productoId.Nombre;
+                    ProductoDesc.Text = productoId.Descripcion;
+                    ProductoPrecio.Text = productoId.Precio;
+                    ProductoStock.Value = productoId.Stock;
+                    ProductoEstado.Checked = productoId.Estado;
+
+                    dgvRecords.DataSource = query.ToList();
+                }
+                else
+                {
+                    MessageBox.Show("No hay registros!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Actualizar
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Models.Producto producto = new Models.Producto();
+                var query = from prod in db.Productos
+                            where prod.ProductoId == (int)ProductoId.Value
+                            select prod;
+
+                Models.Producto producto = query.SingleOrDefault();
+
+                if (producto != null)
+                {
+                    producto.ProductoId = (int)ProductoId.Value;
+                    producto.Nombre = ProductoNom.Text;
+                    producto.Descripcion = ProductoDesc.Text;
+                    producto.Precio = ProductoPrecio.Text;
+                    producto.Stock = (int)ProductoStock.Value;
+                    producto.Estado = ProductoEstado.Checked;
+                    producto.Fecha = DateTime.Now;
+
+                    //db.Productos.Add(producto);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Producto actualzado!");
+                    ResetForm();
+                }
+                else
+                {
+                    MessageBox.Show("Registro no actualizado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Eliminar
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Models.Producto producto = new Models.Producto();
+                var query = from prod in db.Productos
+                            where prod.ProductoId == (int)ProductoId.Value
+                            select prod;
+
+                Models.Producto producto = query.SingleOrDefault();
+
+                if (producto != null)
+                {
+                    producto.ProductoId = (int)ProductoId.Value;
+                    db.Productos.Remove(producto);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Producto Eliminado!");
+                    ResetForm();
+                }
+                else
+                {
+                    MessageBox.Show("Registro no Eliminado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Buscar por nombre
+        private void btnBuscarPorNombre_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var query = from prod in db.Productos
+                            where prod.Nombre.Equals(ProductoNom.Text, StringComparison.OrdinalIgnoreCase)
+                            orderby prod.Nombre ascending
+                            select prod;
+
+                Models.Producto productoId = query.SingleOrDefault();
+
+                if (productoId != null)
+                {
+                    ProductoId.Value = productoId.ProductoId;
+                    ProductoNom.Text = productoId.Nombre;
+                    ProductoDesc.Text = productoId.Descripcion;
+                    ProductoPrecio.Text = productoId.Precio;
+                    ProductoStock.Value = productoId.Stock;
+                    ProductoEstado.Checked = productoId.Estado;
+
+                    dgvRecords.DataSource = query.ToList();
+                }
+                else
+                {
+                    MessageBox.Show("No hay registros!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Filtro por nombres
+        private void btnBuscarFiltro_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var query = from prod in db.Productos
+                            where prod.Nombre.Contains(ProductoNom.Text)
+                            orderby prod.Nombre ascending
+                            select prod;
+
+                dgvRecords.DataSource = query.ToList();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Buscar dando click en el producto
+        private void dgvRecords_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvRecords.CurrentRow.Index != -1)
+            {
+                try
+                {
+                    var Id = Convert.ToInt32(dgvRecords.CurrentRow.Cells["ProductoId"].Value);
+                    var query = from prod in db.Productos
+                                where prod.ProductoId == Id
+                                select prod;
+
+                    Models.Producto productoId = query.SingleOrDefault();
+
+                    if (productoId != null)
+                    {
+                        ProductoId.Value = productoId.ProductoId;
+                        ProductoNom.Text = productoId.Nombre;
+                        ProductoDesc.Text = productoId.Descripcion;
+                        ProductoPrecio.Text = productoId.Precio;
+                        ProductoStock.Value = productoId.Stock;
+                        ProductoEstado.Checked = productoId.Estado;
+
+                        dgvRecords.DataSource = query.ToList();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No hay registros!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
